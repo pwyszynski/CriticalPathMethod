b44ba3d8120f2c8246cbb47dc8fcbf548545b0bd
pwyszynski <stuntmankg@gmail.com>
2014-11-02 17:55:43 +0100

    Hardcoded exemplary nodes, commented out user input

--- cpm.rb
+++ cpm.rb
@@ -1,7 +1,7 @@
 require 'graph'
 
 class Node <
-	Struct.new(:index, :czas, :poprzednik)
+	Struct.new(:index, :czas, :poprzednik, :est)
 
 	def print_node
 		p "#{index} | #{czas} | #{poprzednik}"
@@ -14,18 +14,30 @@ graf = Array.new
 puts "Podaj liczbę węzłów:" 
 n = gets.chomp.to_i
 
-for i in (0...n) do
-	wz = Node.new
-	p "#{i}. indeks: "
-	wz.index = gets.chomp
-	p "#{i}. czas: "
-	wz.czas = gets.chomp.to_i
-	p "#{i}. poprzednicy: "
-	wz.poprzednik = gets.chomp.split(' ').map(&:to_i)
 
-	graf.push(wz)
-end
+#Input przez usera:
+# for i in (0...n) do
+# 	wz = Node.new
+# 	p "#{i}. indeks: "
+# 	wz.index = gets.chomp
+# 	p "#{i}. czas: "
+# 	wz.czas = gets.chomp.to_i
+# 	p "#{i}. poprzednicy: "
+# 	wz.poprzednik = gets.chomp.split(' ').map(&:to_i)
 
+# 	graf.push(wz)
+# end
+
+graf.push(Node.new(1, 3, "".split(' ').map(&:to_i))
+graf.push(Node.new(2, 1, "1".split(' ').map(&:to_i))
+graf.push(Node.new(3, 4, "1".split(' ').map(&:to_i))
+graf.push(Node.new(4, 1, "2 3".split(' ').map(&:to_i))
+graf.push(Node.new(5, 9, "4".split(' ').map(&:to_i))
+graf.push(Node.new(6, 5, "4".split(' ').map(&:to_i))
+graf.push(Node.new(7, 2, "5 6".split(' ').map(&:to_i))
+
+
+graf.sort! { |a,b| a.index <=> b.index }
 graf.each do |x|
 	x.print_node
 end
