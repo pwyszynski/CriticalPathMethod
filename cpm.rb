f96cfa7b2cb0d38318326b87025e39a521318ba8
pwyszynski <stuntmankg@gmail.com>
2014-11-02 19:04:38 +0100

    Probably working stuff

--- cpm.rb
+++ cpm.rb
@@ -1,10 +1,10 @@
 require 'graph'
 
 class Node <
-	Struct.new(:index, :czas, :poprzednik, :est)
+	Struct.new(:index, :czas, :poprzednik, :est, :eet)
 
 	def print_node
-		p "#{index} | #{czas} | #{poprzednik}"
+		p "#{index} | #{czas} | #{poprzednik} | #{est} | #{eet}"
 	end
 end
 
@@ -28,13 +28,14 @@ n = gets.chomp.to_i
 # 	graf.push(wz)
 # end
 
-graf.push(Node.new(1, 3, "".split(' ').map(&:to_i))
-graf.push(Node.new(2, 1, "1".split(' ').map(&:to_i))
-graf.push(Node.new(3, 4, "1".split(' ').map(&:to_i))
-graf.push(Node.new(4, 1, "2 3".split(' ').map(&:to_i))
-graf.push(Node.new(5, 9, "4".split(' ').map(&:to_i))
-graf.push(Node.new(6, 5, "4".split(' ').map(&:to_i))
-graf.push(Node.new(7, 2, "5 6".split(' ').map(&:to_i))
+#Hardcoded input:
+graf.push(Node.new(1, 3, "".split(' ').map(&:to_i)))
+graf.push(Node.new(2, 1, "1".split(' ').map(&:to_i)))
+graf.push(Node.new(3, 4, "1".split(' ').map(&:to_i)))
+graf.push(Node.new(4, 1, "2 3".split(' ').map(&:to_i)))
+graf.push(Node.new(5, 9, "4".split(' ').map(&:to_i)))
+graf.push(Node.new(6, 5, "4".split(' ').map(&:to_i)))
+graf.push(Node.new(7, 2, "5 6".split(' ').map(&:to_i)))
 
 
 graf.sort! { |a,b| a.index <=> b.index }
@@ -42,6 +43,44 @@ graf.each do |x|
 	x.print_node
 end
 
+numofnodes = graf.length
+criticalnodes = ""
+
+
+for i in (0...numofnodes) do
+	if graf[i].poprzednik.length == 0 then
+		graf[i].est = 0
+		graf[i].eet = graf[i].czas
+		criticalnodes = criticalnodes + "#{graf[i].index}"
+	end
+	if graf[i].poprzednik.length == 1 then
+		poprz = graf[i].poprzednik[0].to_i
+		graf[i].est = graf[poprz-1].eet
+		graf[i].eet = graf[i].est + graf[i].czas
+		criticalnodes = criticalnodes + "#{graf[i].index}"
+	end
+	if graf[i].poprzednik.length > 1 then
+		dlugpoprzed = graf[i].poprzednik.length
+		indmax = 0 
+		for j in (0...dlugpoprzed) do
+			indpom = graf[i].poprzednik[j]-1
+			if graf[indpom].eet > graf[indmax].eet
+				indmax = indpom
+			end
+		end
+		graf[i].est = graf[indmax].eet
+		criticalnodes = criticalnodes + "#{graf[i].index}"
+		graf[i].eet = graf[i].est + graf[i].czas
+	end
+end
+
+p "\n\n"
+p "#{criticalnodes}"
+p "\n\n"
+
+graf.each do |x|
+	x.print_node
+end
 # digraph do
 #       # many ways to access/create edges and nodes
 #       edge "a", "b"
